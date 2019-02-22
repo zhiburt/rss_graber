f37dd26666c2ded6cd1f693960f03430940ed8c0
Maxim Zhiburt <zhiburt@gmail.com>
2019-02-22 13:29:52 +0300

    Refactor ServerRender method

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using web.Context;
 using web.Models;
 
@@ -20,7 +21,7 @@ namespace web.Controllers
     public class HomeController : Controller
     {
         private readonly RSSContext _rssContext;
-        private const int MAX_ELEMENTS_ON_PAGE = 15;
+        private const int MAX_ELEMENTS_ON_PAGE = 10;
 
         public HomeController(RSSContext rSSContext)
         {
@@ -29,27 +30,73 @@ namespace web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("ServerRender");
         }
 
         public IActionResult ServerRender(SortType sortOrder, string selectOnly, int page = 1)
         {
             var model = _rssContext.Set<RSS>().ToList();
             ViewData["SelectList"] = model.Select(rss => resourceName(rss.LinkToOriginal)).Distinct();
-            
-            if (!string.IsNullOrEmpty(selectOnly) || !string.IsNullOrEmpty(ViewData["selectOnly"] as string))
+
+            var onlyResorsec = SelectOnlyByResource(model, selectOnly, ViewData["selectOnly"] as string);
+            if (onlyResorsec != null)
+            {
+                model = onlyResorsec;
+                ViewData["selectOnly"] = selectOnly;
+            }
+
+            model = SortBy(model, sortOrder);
+            ViewData["sortOrder"] = (int)sortOrder;
+
+            (model, ViewData["PriviousPage"], ViewData["NextPage"]) =  Pagination(model, page);
+
+            return View(model);
+        }
+
+        private (List<RSS> newmodel, int privious, int next) Pagination(List<RSS> model, int page)
+        {
+            int privious = -1;
+            int next = -1;
+
+            int devide = model.Count / MAX_ELEMENTS_ON_PAGE;
+            if (devide > 0)
+            {
+                var skiped = model.Skip(((page * MAX_ELEMENTS_ON_PAGE) - MAX_ELEMENTS_ON_PAGE));
+                if (skiped.Count() > MAX_ELEMENTS_ON_PAGE)
+                {
+                    model = skiped.Take(MAX_ELEMENTS_ON_PAGE).ToList();
+                    next = page + 1;
+                }
+                else
+                {
+                    model = skiped.ToList();
+                }
+
+                privious = page - 1;
+            }
+
+            return (model, privious, next);
+        }
+
+        private List<RSS> SelectOnlyByResource(List<RSS> model, string selectOnly, string selectOnlyCashe)
+        {
+            if (!string.IsNullOrEmpty(selectOnly) || !string.IsNullOrEmpty(selectOnlyCashe))
             {
                 model = model.Select(rss => rss)
                     .Where(r => resourceName(r.LinkToOriginal) == selectOnly)
                     .ToList();
-                ViewData["selectOnly"] = selectOnly;
+                return model;
             }
 
+            return null;
+        }
+
+        private List<RSS> SortBy(List<RSS> model, SortType sortOrder)
+        {
             switch (sortOrder)
             {
                 case SortType.ByDate:
                     model = model.OrderBy(rss => rss.Date).ToList();
-
                     break;
                 case SortType.ByLink:
                     model = model.OrderBy(rss => resourceName(rss.LinkToOriginal)).ToList();
@@ -61,28 +108,12 @@ namespace web.Controllers
                     break;
             }
 
-            ViewData["sortOrder"] = (int)sortOrder;
+            return model;
+        }
 
-            ViewData["NextPage"] = -1;
-            ViewData["PriviousPage"] = -1;
-            int devide = model.Count / MAX_ELEMENTS_ON_PAGE;
-            if (devide > 0)
-            {
-                var skiped = model.Skip(((page * MAX_ELEMENTS_ON_PAGE) - MAX_ELEMENTS_ON_PAGE));
-                if (skiped.Count() > MAX_ELEMENTS_ON_PAGE)
-                {
-                    model = skiped.Take(MAX_ELEMENTS_ON_PAGE).ToList();
-                    ViewData["NextPage"] = page+1;
-                }
-                else
-                {
-                    model = skiped.ToList();
-                }
-
-                ViewData["PriviousPage"] = page-1;
-            }
-
-            return View(model);
+        public IActionResult ClientRender()
+        {
+            return View(_rssContext.Set<RSS>().AsNoTracking());
         }
 
         public IActionResult Contact()
